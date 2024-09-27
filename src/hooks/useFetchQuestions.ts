8d27045acd4e10a1c07d5383327aa17b121d03dc
hiroTochigi <hiroyuki.terauchi.japan@gmail.com>
2024-09-26 21:33:13 -0500

    feat: fetch all questions from backend and remove dummy data
    - Implemented API call to retrieve all questions from the backend.
    - Removed previously hardcoded dummy questions to rely on dynamic data.

--- src/hooks/useFetchQuestions.ts
+++ src/hooks/useFetchQuestions.ts
@@ -1,43 +1,27 @@
+import { getData } from "@/lib/api";
 import { useEffect, useState } from "react";
 
-const initQuestions: Question[] = [{
-    id: 1,
-    title: 'Question 1',
-    explanation: 'Explanation 1'
-}, {
-    id: 2,
-    title: 'Question 2',
-    explanation: 'Explanation 2'
-}, {
-    id: 3,
-    title: 'Question 3',
-    explanation: 'Explanation 3'
-}, {
-    id: 4,
-    title: 'Question 4',
-    explanation: 'Explanation 4'
-}]
-
 const useFetchQuestions = () => {   
-    const [questions, setQuestions] = useState<Question[]>(initQuestions);
+    const [questions, setQuestions] = useState<Question[]>([]);
     const [loading, setLoading] = useState<boolean>(true);
     const [error, setError] = useState<string | null>(null);
     
-    /*useEffect(() => {
+    useEffect(() => {
         setLoading(true);
-        fetch('https://opentdb.com/api.php?amount=10')
-        .then((response) => response.json())
-        .then((data) => {
-            setQuestions(data.results);
+        getData('/questions')
+        .then((response: any) => {
+            setQuestions(response);
             setLoading(false);
+            return response
         })
         .catch((error) => {
+            alert(error)
             setError(error.message);
             setLoading(false);
         });
-    }, []);*/
+    }, []);
     
     return { questions, loading, error };
-    };
+};
     
     export default useFetchQuestions
